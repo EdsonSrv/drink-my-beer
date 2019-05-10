a61c5bc0328c8f5e98278260dd9a3c41d3bba9a0
EdsonSrv <edson@ciencias.unam.mx>
2019-05-10 12:37:42 -0500

    Adding beer atributes

--- app/models/beer.js
+++ app/models/beer.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-
-});
+export default DS.Model.extend(
+  {id:1, name:"fake name 1", price:36, location:"invited", type: "type fake"},
+  {id:2, name:"fake name 2", price:36, location:"canceled", type: "type fake"},
+  {id:3, name:"fake name 3", price:36, location:"reinvited", type: "type fake"},
+  {id:4, name:"fake name 4", price:36, location:"invited", type: "type fake"}
+);
