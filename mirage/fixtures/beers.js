fcbc7d94994f409c963101609b7b0b704682f1af
EdsonSrv <edson@ciencias.unam.mx>
2019-05-10 12:53:20 -0500

    Create body beer and insert beers on fixtures

--- mirage/fixtures/beers.js
+++ mirage/fixtures/beers.js
@@ -1,2 +1,6 @@
 export default [
+  {id:1, name:"fake name 1", price:36, location:"invited", type: "type fake"},
+  {id:2, name:"fake name 2", price:36, location:"canceled", type: "type fake"},
+  {id:3, name:"fake name 3", price:36, location:"reinvited", type: "type fake"},
+  {id:4, name:"fake name 4", price:36, location:"invited", type: "type fake"}
 ];
