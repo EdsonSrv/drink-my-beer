03f86f6b3c371dfcdf5070a02101c958ca692f43
EdsonSrv <edson@ciencias.unam.mx>
2019-05-13 10:40:41 -0500

    #2 Se agregó formulario para compartir una nueva cerveza

--- app/routes/agregar-cerveza.js
+++ app/routes/agregar-cerveza.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  model(){
+
+  },
+  actions:{
+    saveBeer: function(){
+        alert("Action");
+    }
+  }
+
 });
