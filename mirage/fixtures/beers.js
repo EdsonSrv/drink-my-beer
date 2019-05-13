99aa60a4c11f12a76ed2fbc23a8d96ed181f1c46
EdsonSrv <edson@ciencias.unam.mx>
2019-05-13 09:57:14 -0500

    #2 Se agregó datos con mirage y se mostraron en hbs, se anidaron las vistas

--- mirage/fixtures/beers.js
+++ mirage/fixtures/beers.js
@@ -1,6 +1,6 @@
 export default [
-  {id:1, name:"fake name 1", price:36, location:"invited", type: "type fake"},
-  {id:2, name:"fake name 2", price:36, location:"canceled", type: "type fake"},
-  {id:3, name:"fake name 3", price:36, location:"reinvited", type: "type fake"},
-  {id:4, name:"fake name 4", price:36, location:"invited", type: "type fake"}
+  {id:1, name:"fake name 1", price:36, location:"loaction 1", type: "type fake"},
+  {id:2, name:"fake name 2", price:96, location:"location 2", type: "type fake"},
+  {id:3, name:"fake name 3", price:101, location:"location 3", type: "type fake"},
+  {id:4, name:"fake name 4", price:58, location:"location 4", type: "type fake"}
 ];
